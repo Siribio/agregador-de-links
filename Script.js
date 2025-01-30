707897b851384ac95b78bea771046b7d9522a88c
Siribio <osilvio123@gmail.com>
2025-01-29 23:02:23 -0300

    final (1.0)

---
+++ Script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement
+  
+
+  // if (html.classList.contains('light')) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+  html.classList.toggle("light") 
+
+
+//pegar a tag img
+const img = document.querySelector("#profile img")
+
+if(html.classList.contains("light")){
+  
+  
+  img.setAttribute("src", "./assets/higuruma-badass.png")
+} else{
+  img.setAttribute("src","./assets/higuruma.png")
+
+}
+
+//substituir a imagem
+//se tiver light mode adicionar imagem light
+
+}
